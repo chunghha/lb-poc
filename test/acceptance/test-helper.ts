61ef1b864f0afad891b40befc6161623379d8c59
Chris Ha <chunghha@users.noreply.github.com>
2018-10-19 04:25:51 -0500

    initial commit

---
+++ test/acceptance/test-helper.ts
@@ -0,0 +1,24 @@
+import {LbPocApplication} from '../..';
+import {
+  createRestAppClient,
+  givenHttpServerConfig,
+  Client,
+} from '@loopback/testlab';
+
+export async function setupApplication(): Promise<AppWithClient> {
+  const app = new LbPocApplication({
+    rest: givenHttpServerConfig(),
+  });
+
+  await app.boot();
+  await app.start();
+
+  const client = createRestAppClient(app);
+
+  return {app, client};
+}
+
+export interface AppWithClient {
+  app: LbPocApplication;
+  client: Client;
+}
