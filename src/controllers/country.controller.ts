e865ebe792ea7d6a6981dfe47b60e774b62d2c6d
Chris Ha <chunghha@users.noreply.github.com>
2018-10-22 21:11:01 -0500

    chore: remove unnecessary repository

--- src/controllers/country.controller.ts
+++ src/controllers/country.controller.ts
@@ -2,19 +2,15 @@ import { get } from '@loopback/rest';
 import { repository } from '@loopback/repository';
 
 import { CountryService, CountryServiceProvider } from '../services';
-import { CountryRepository } from '../repositories';
 import { Country } from '../models';
 
 export class CountryController {
 
   constructor(
-    @repository(CountryRepository)
-    public countryRepository: CountryRepository,
-
     protected countryServiceProvider: CountryServiceProvider = new CountryServiceProvider()) { }
 
   // Map to `GET /country`
-  @get('/country', {
+  @get('/countries', {
     responses: {
       '200': {
         description: 'Array of Country model instance',
@@ -24,14 +20,14 @@ export class CountryController {
               type: 'array',
               items: {
                 'x-ts-type': Country
-              },
+              }
             },
           },
         },
       },
     },
   })
-  async country(): Promise<Country[]> {
+  async getCountries(): Promise<Country[]> {
     const countryService: CountryService = await this.countryServiceProvider.value();
 
     return await countryService.getCountries();
