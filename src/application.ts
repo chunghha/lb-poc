25a75c5eb7a55b431dd77b77efcb54cdf0755bb7
Chris Ha <chunghha@users.noreply.github.com>
2018-11-17 16:33:27 -0600

    feat: self-hosted REST API Explorer

--- src/application.ts
+++ src/application.ts
@@ -2,7 +2,13 @@ import { BootMixin } from '@loopback/boot';
 import { ApplicationConfig } from '@loopback/core';
 import { RepositoryMixin } from '@loopback/repository';
 import { RestApplication } from '@loopback/rest';
+import {
+  RestExplorerBindings,
+  RestExplorerComponent
+} from '@loopback/rest-explorer';
 import { ServiceMixin } from '@loopback/service-proxy';
+
+import * as path from 'path';
 
 import { MySequence } from './sequence';
 
@@ -14,6 +20,15 @@ export class LbPocApplication extends BootMixin(
 
     // Set up the custom sequence
     this.sequence(MySequence);
+
+    // Set up default home page
+    this.static('/', path.join(__dirname, '../../public'));
+
+    // Customize @loopback/rest-explorer configuration here
+    this.bind(RestExplorerBindings.CONFIG).to({
+      path: '/explorer',
+    });
+    this.component(RestExplorerComponent);
 
     this.projectRoot = __dirname;
     // Customize @loopback/boot Booter Conventions here
