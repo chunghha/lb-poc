a553bccff8604d288cf79be9f63c389076cd51f5
Chris Ha <chunghha@users.noreply.github.com>
2019-12-09 13:13:50 -0600

    fix: datasource config json

--- src/datasources/country.datasource.ts
+++ src/datasources/country.datasource.ts
@@ -1,7 +1,7 @@
 import { inject } from '@loopback/core';
 import { juggler } from '@loopback/repository';
 
-import * as config from './country.datasource.json';
+import * as config from './country.datasource.config.json';
 
 export class CountryDataSource extends juggler.DataSource {
   static dataSourceName = 'country';
